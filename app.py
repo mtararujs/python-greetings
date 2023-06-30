a07439908a5bf5832b16178e272c07247e961d66
Martins Tararujs <martins@Martinss-MacBook-Pro-2.local>
2023-06-30 12:47:52 +0300

    Fix app

--- app.py
+++ app.py
@@ -7,9 +7,4 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', default=5000, type=int, help='Port to run the server on.')
-    args = parser.parse_args()
-    
-    app.run(debug=True, port=args.port)
+    app.run(host="0.0.0.0", debug=True, port="3000")
