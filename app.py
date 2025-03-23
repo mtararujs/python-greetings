d3cc96735a32c814bb87dc7e77eac87feb15fcb4
Mārtiņš Tararujs <martins.tararujs@testdevlab.com>
2025-03-23 14:54:39 +0200

    Add ability to use custom ports

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import argparse
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -7,4 +9,8 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=int(os.getenv("PORT", 3000)), help='Port to run the app')
+    args = parser.parse_args()
+    
+    app.run(host="0.0.0.0", debug=True, port=args.port)
